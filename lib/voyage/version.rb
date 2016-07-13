c15816703a7d13f1c50d7e1431ad35f2e2d46a37
Jon Kinney <jonkinney@gmail.com>
2016-07-12 22:12:03 -0500

    Update Voyage to Suspenders 1.40

--- lib/voyage/version.rb
+++ lib/voyage/version.rb
@@ -1,8 +1,9 @@
 module Voyage
   RAILS_VERSION = "~> 4.2".freeze
-  RUBY_VERSION = IO.
-    read("#{File.dirname(__FILE__)}/../../.ruby-version").
-    strip.
-    freeze
-  VERSION = '1.0'.freeze
+  RUBY_VERSION =
+    IO
+      .read("#{File.dirname(__FILE__)}/../../.ruby-version")
+      .strip
+      .freeze
+  VERSION = '1.1'.freeze
 end
