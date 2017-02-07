75bfc5f51b33681ee58fb53609bcb8c698d4b162
Jon Kinney <jonkinney@gmail.com>
2017-02-06 22:22:08 -0600

    Add overrides for app_builder and app_generator
    Add overrides for app_builder and app_generator
    
    Enforce indentation of multiline method calls
    
    Finalize switch to Foundation
    
    Switch webpacker to rubygems instead of GitHub
    
    Convert to modern scss and add base margin
    
    Update voyage README with gemset usage instructions
    
    Add new template files for Sidekiq config
    
    Add sidekiq and redis-namespace gems
    
    Add sidekiq configuration to environments and copy in config file and initializer
    
    Add Redis requirement to README
    
    Add administrate gems
    
    Install administrate and setup our custom controllers
    
    Customize user dashboard, add routes
    
    Add files for Administrate RolesField
    
    Add new Trix field gem
    
    Add Trix gem setup
    
    Add Administrate helper for hiding resources in sidebar
    
    Move Administrate behind option
    
    Add Administrate views for impersonation
    
    Make Administrate install non-optional
    
    Integrate active_link_to and add a basic active style
    
    Switch links to named routes
    
    Use letter_opener for mail delivery in development
    
    Setup erd and add base config
    
    Configure post-migration hook for ERD
    
    Add cucumber
    
    Disable serializers, Fixes #19
    
    All the Cucumber things
    
    CanCan redirect to static page, Fixes #46
    
    User Password Fields
    
    Administrate Field tweaks
    
    Fix cucumber file path
    
    More cucumber path fix
    
    Replace carrierwave with shrine, Fixes #25
    
    User Profile Photo Upload from Administrate

---
+++ lib/voyage.rb
@@ -0,0 +1,13 @@
+require 'voyage/version'
+
+require 'suspenders/generators/app_generator'
+require "suspenders/generators/enforce_ssl_generator"
+require "suspenders/generators/initialize_active_job_generator"
+require 'suspenders/generators/static_generator'
+require 'suspenders/generators/stylesheet_base_generator'
+require 'suspenders/actions'
+require "suspenders/adapters/heroku"
+require 'suspenders/app_builder'
+
+require 'voyage/generators/app_generator'
+require 'voyage/app_builder'
