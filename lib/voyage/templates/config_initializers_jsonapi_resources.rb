8d959306c0ce80616883a8e1c2ffe46cf0fcf320
Jon Kinney <jonkinney@gmail.com>
2017-02-06 22:22:08 -0600

    Add overrides for app_builder and app_generator

---
+++ lib/voyage/templates/config_initializers_jsonapi_resources.rb
@@ -0,0 +1,7 @@
+JSONAPI.configure do |config|
+  config.json_key_format = :underscored_key
+  config.route_format = :underscored_route
+
+  config.top_level_meta_include_record_count = true
+  config.top_level_meta_record_count_key = :record_count
+end
