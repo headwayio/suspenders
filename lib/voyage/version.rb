769c491b1c104ca2373cc5bca0d3fd9618a3eba6
Jon Kinney <jonkinney@gmail.com>
2017-02-06 22:22:08 -0600

    Add overrides for app_builder and app_generator

---
+++ lib/voyage/version.rb
@@ -0,0 +1,9 @@
+module Voyage
+  RAILS_VERSION = '~> 5.1.0.rc1'.freeze
+  RUBY_VERSION =
+    IO
+      .read("#{File.dirname(__FILE__)}/../../.ruby-version")
+      .strip
+      .freeze
+  VERSION = '1.44.0.10'.freeze
+end
