5f7b28bef475329e82177cbfbeaef9d6e46a4732
Noah Settersten <noah@headway.io>
2017-10-23 14:16:54 -0500

    Replace factory_girl with factory_bot

--- templates/cucumber/features/support/factory_bot.rb
+++ templates/cucumber/features/support/factory_bot.rb
@@ -1,9 +1,9 @@
 require 'faker'
-require 'factory_girl_rails'
+require 'factory_bot_rails'
 
 # ensure Spring has the latest version of the factories
-FactoryGirl.reload
+FactoryBot.reload
 
 # Allow factories to be created with just create(:user) instead of
-# FactoryGirl.create(:user)
-World(FactoryGirl::Syntax::Methods)
+# FactoryBot.create(:user)
+World(FactoryBot::Syntax::Methods)
