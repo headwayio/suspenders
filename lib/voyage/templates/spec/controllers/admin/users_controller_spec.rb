8a26a9a38f371e89939e0859d21d37c624f4b35b
Noah Settersten <noah@headway.io>
2017-11-07 12:54:42 -0600

    Add shared example spec for Administrate dashboards

--- lib/voyage/templates/spec/controllers/admin/users_controller_spec.rb
+++ lib/voyage/templates/spec/controllers/admin/users_controller_spec.rb
@@ -1,40 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :request do
-  let(:admin_user) { create(:user, :admin) }
-  let(:user) { create(:user) }
-
-  describe '#index' do
-    context 'authenticated' do
-      context 'admin' do
-        it 'loads all users in the browser' do
-          sign_in(admin_user)
-          get admin_users_path
-          expect(response).to be_success
-        end
-      end
-
-      context 'user' do
-        it 'redirects with an alert that you need to be an admin' do
-          sign_in(user)
-          get admin_users_path
-          expect(response).to be_redirect
-
-          txt = 'You must be an admin to perform that action'
-          expect(flash[:alert]).to eq(txt)
-        end
-      end
-    end
-
-    context 'NOT authenticated' do
-      it 'redirects to sign in page with an alert' do
-        get admin_users_path
-        expect(response).to be_redirect
-
-        txt = 'You need to sign in or sign up before continuing.'
-        expect(flash[:alert]).to eq(txt)
-      end
-    end
+  it_behaves_like 'dashboard' do
+    let(:path) { admin_users_path }
   end
 
   describe '#impersonate' do
